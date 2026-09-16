536d09068a3cf10a4ea3cf0c6fb574ff8e79c37b
wenzhengcheng <wenzhengcheng0223@gmail.com>
2026-09-17 05:49:07 +0800

    add development package

--- modules/home-manager/packages/development.nix
+++ modules/home-manager/packages/development.nix
@@ -4,9 +4,19 @@
   home.packages = with pkgs; [
     gcc
     gnumake
+    
+    go
+    jdk
+    jetbrains.datagrip
+    jetbrains.goland
+    jetbrains.idea
+
     pkg-config
     vscode
     python3
+
+    nodejs_24
+    pnpm_11
+
   ];
 }
-
